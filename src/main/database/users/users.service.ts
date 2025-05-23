f643aa072f51664e3208c4b0a1e4d1108645c5ee
h4ckx0r <natanagoor@gmail.com>
2025-05-23 17:17:47 +0200

    feat(setup): Al crear el primer usuario se crea un rol de administrador y se le asigna automáticamente

--- src/main/database/users/users.service.ts
+++ src/main/database/users/users.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UserEntity } from '../models/entity/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { UserDto } from '../models/dto/user.dto';
+import { Repository, DeleteResult } from 'typeorm';
+import { UserDto, UserResponseDto } from '../models/dto/user.dto';
+import { RoleDto } from '../models/dto/role.dto';
+import { PermissionLevel } from '../models/entity/role.entity';
+import { RolesService } from '../roles/roles.service';
+import { PageDto } from '../models/dto/page.dto';
+import { PageOptionsDto } from '../models/dto/pageOptions.dto';
+import { ORDER_OPTIONS } from '../models/dto/pageOptions.dto';
 
 @Injectable()
 export class UsersService {
@@ -26,7 +32,40 @@ export class UsersService {
             throw new Error('El usuario ya existe');
         }
         
-        const user = this.userRepository.create(UserDto.toEntity(userDto));
+        const userCount = await this.userRepository.count();
+        if (userCount === 0) {
+            const role = new RoleDto();
+            role.name = 'Administrador';
+            role.adminUsers = [PermissionLevel.READ, PermissionLevel.WRITE, PermissionLevel.DELETE];
+            role.adminSystem = [PermissionLevel.READ, PermissionLevel.WRITE, PermissionLevel.DELETE];
+            role.adminRoles = [PermissionLevel.READ, PermissionLevel.WRITE, PermissionLevel.DELETE];
+            const roleSaved = await this.rolesService.create(role);
+            userDto.roles = [roleSaved];
+        }
+        
+
+        const { id, ...userDtoSinId } = userDto;
+        const userSaved = await this.userRepository.save(userDtoSinId);
+        return UserDto.fromEntity(userSaved);
+    }
+
+    async deleteOne(id: string): Promise<DeleteResult> {
+        const user = await this.findOne(id);
+        if (!user) {
+            throw new Error('El usuario no existe');
+        }
+        const deletedUser = await this.userRepository.delete(id);
+        return deletedUser;
+    }
+
+    async updateOne(id: string, userDto: UserDto): Promise<UserDto> {
+        const user = await this.findOne(id);
+        if (!user) {
+            throw new Error('El usuario no existe');
+        }
+        userDto.password = undefined;
+        Object.assign(user, userDto);
+
         const userSaved = await this.userRepository.save(user);
         return UserDto.fromEntity(userSaved);
     }
