3e4bda33b46c5e2e17d9712d7b34af047ff1efb7
Natanael Gómez <natanagoor@icloud.com>
2025-05-26 19:56:41 +0000

    feat(docs): OpenAPI y Swagger añadido

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('BackToBackup API')
+    .setDescription('BackToBackup API description')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, configSwagger);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
